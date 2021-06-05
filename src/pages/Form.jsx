7d2dadf69329feebb84a572b102f16df0302820b
Jonathan Rocha <jonathanferreira.contato@gmail.com>
2021-06-05 01:16:11 -0300

    starting form

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,13 +1,43 @@
 import React from "react";
 
 class Form extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+
+      name: "",
+      surName: "",
+      birthDate: "",
+
+      fullAddress: "",
+    };
+  }
+
   render() {
     return (
       <div>
-        <h1>Hello Form</h1>
+        <ul>
+          <li>first step</li>
+          <li>second step</li>
+          <li>third step</li>
+        </ul>
       </div>
     );
   }
 }
 
 export default Form;
+
+// A primeira etapa deve solicitar:
+// Email (obrigatório)
+// Senha (obrigatório)
+// Confirmação de senha (obrigatório)
+// A segunda etapa deve solicitar:
+// Nome (obrigatório)
+// Sobrenome (obrigatório)
+// Data de nacimento
+// A terceira etapa deve solicitar:
+// Endereço completo (obrigatório)
